389c387a26f039d5193b58688a0b37f7d0bd9b98
andrewboyley <andy.boyley@gmail.com>
2022-10-27 20:18:46 +0200

    Fixed model saving

--- sb3/sb3_ppolstm_minihack.py
+++ sb3/sb3_ppolstm_minihack.py
@@ -1,14 +1,14 @@
-from pydoc import cli
+
 import gym
 import minihack
 import time
 
 import numpy as np
 
-from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecMonitor
+from stable_baselines3.common.vec_env import DummyVecEnv, VecMonitor
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.utils import set_random_seed
-from stable_baselines3 import A2C
+from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from sb3_contrib import RecurrentPPO
 
@@ -16,30 +16,51 @@ import torch
 from torch import nn
 
 
-def evaluate_lstm(model, env_id):
-    num_envs = 1
-    env = make_vec_env(env_id, n_envs=num_envs, vec_env_cls=DummyVecEnv)
-    obs = env.reset()
+def evaluate_lstm(model, eval_env):
+    
+    obs = eval_env.reset()
 
     # cell and hidden state of the LSTM
     lstm_states = None
     # Episode start signals are used to reset the lstm states
-    episode_starts = np.ones((num_envs, ), dtype=bool)
+    episode_starts = np.ones((1, ), dtype=bool)
     for _ in range(2000):
         action, lstm_states = model.predict(obs,
                                             state=lstm_states,
                                             episode_start=episode_starts,
                                             deterministic=True)
 
-        obs, rewards, dones, info = env.step(action)
+        obs, rewards, dones, info = eval_env.step(action)
 
         episode_starts = dones
-        env.render()
+        eval_env.render()
+        time.sleep(0.1)
 
         if dones:
             time.sleep(1)
-            obs = env.reset()
+            obs = eval_env.reset()
 
+class SaveCallback(BaseCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq:
+    :param log_dir: Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: Verbosity level: 0 for no output, 1 for info messages, 2 for debug messages
+    """
+
+    def __init__(self, check_freq: int, verbose: int = 1):
+        super(SaveCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+            print(f'Saving model at step {self.num_timesteps}')
+            # self.model.save('model')
+
+        return True
 
 class MiniHackExtractor(BaseFeaturesExtractor):
 
@@ -125,46 +146,53 @@ def make_env(env_id, rank, seed=0, args={}):
     set_random_seed(seed)
     return _init
 
+def make_dummy_env(env_id, num_envs):
+    args = {
+            'observation_keys': ['chars', 'chars_crop'],
+            'penalty_time': -0.005,
+            'penalty_step': -0.1
+        }
+
+    # Create the vectorized environment
+    env = DummyVecEnv([make_env(env_id, i, args=args) for i in range(num_envs)])
+    return env
 
 if __name__ == "__main__":
     env_id = "MiniHack-River-v0"
-    args = {
-        'observation_keys': ['chars', 'chars_crop'],
-        'penalty_time': -0.005,
-        'penalty_step': -0.1
-    }
-
-    num_cpu = 12  # Number of processes to use
-    # Create the vectorized environment
-    env = DummyVecEnv([make_env(env_id, i, args=args) for i in range(num_cpu)])
+    n_envs = 12
+    env = make_dummy_env(env_id, n_envs)
 
     # wrap env with a VecMonitor
     env = VecMonitor(env)
 
     model = RecurrentPPO("MultiInputLstmPolicy",
                          env,
-                         learning_rate=0.0013,
+                         learning_rate=5.392404218017658e-05,
                          verbose=1,
-                         gamma=0.9698,
-                         ent_coef=0.000178,
-                         max_grad_norm=1.8557,
-                         n_epochs=1,
-                         batch_size=32,
-                         clip_range=0.1,
-                         n_steps=32,
-                         gae_lambda=0.992,
+                         gamma=0.999,
+                         ent_coef=3.0530546078783706e-07,
+                         max_grad_norm=0.8088001457186847,
+                         n_epochs=10,
+                         batch_size=256,
+                         clip_range=0.3,
+                         vf_coef=0.00011087766732100762,
+                         n_steps=128,
+                         gae_lambda=0.9971223067459642,
                          policy_kwargs=dict(
                              features_extractor_class=MiniHackExtractor,
                              ortho_init=True,
-                             lstm_hidden_size=64,
-                             net_arch=[{
-                                 "pi": [64, 64],
-                                 "vf": [64, 64]
-                             }]))
+                             activation_fn= nn.ELU,
+                             enable_critic_lstm=False,
+                             lstm_hidden_size=256,
+                             net_arch=[{'pi': [64, 64], 'vf': [64, 64]}]))
+
+    save_callback = SaveCallback(max(5000 // n_envs, 1))
 
     try:
-        model.learn(total_timesteps=int(2e5))
+        model.learn(total_timesteps=int(10000), callback=save_callback)
     except KeyboardInterrupt:
         pass
 
-    evaluate_lstm(model, env_id)
+    eval_env = make_dummy_env(env_id, 1)
+
+    evaluate_lstm(model, eval_env)
